5e9c8506702366b59fbcf92063c91b9d3da526b7
norhther <omar.lopez@est.fib.upc.edu>
2026-08-09 21:32:43 +0200

    feat: wire AUv3 extension factory and AudioComponents Info.plist

--- TransposerExtension/TransposerAudioUnitFactory.swift
+++ TransposerExtension/TransposerAudioUnitFactory.swift
@@ -1,7 +1,22 @@
 import CoreAudioKit
 
 public class TransposerAudioUnitFactory: AUViewController, AUAudioUnitFactory {
+    private var audioUnit: TransposerAudioUnit?
+
     public func createAudioUnit(with componentDescription: AudioComponentDescription) throws -> AUAudioUnit {
-        try AUAudioUnit(componentDescription: componentDescription)
+        let audioUnit = try TransposerAudioUnit(componentDescription: componentDescription, options: [])
+        self.audioUnit = audioUnit
+        return audioUnit
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let audioUnit = audioUnit else { return }
+        let hosting = TransposerViewController(audioUnit: audioUnit)
+        addChild(hosting)
+        hosting.view.frame = view.bounds
+        hosting.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(hosting.view)
+        hosting.didMove(toParent: self)
     }
 }
